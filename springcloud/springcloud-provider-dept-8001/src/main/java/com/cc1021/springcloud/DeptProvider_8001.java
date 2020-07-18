219699ad2642840b1fba95cffcd39f59ce8d9e4f
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-18 17:56:29 +0800

    修改eureka名字，配置eureka信息

--- springcloud/springcloud-provider-dept-8001/src/main/java/com/cc1021/springcloud/DeptProvider_8001.java
+++ springcloud/springcloud-provider-dept-8001/src/main/java/com/cc1021/springcloud/DeptProvider_8001.java
@@ -2,11 +2,13 @@ package com.cc1021.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * 启动类
  */
 @SpringBootApplication
+@EnableEurekaClient // 在服务启动后自动注册到 Eureka 中
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
