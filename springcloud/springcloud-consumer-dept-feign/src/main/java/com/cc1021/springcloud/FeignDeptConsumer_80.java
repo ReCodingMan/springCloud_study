3b49ba91c46a9b85b29db5f760f7eb7c34f2f31b
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-20 20:50:15 +0800

    feign init

---
+++ springcloud/springcloud-consumer-dept-feign/src/main/java/com/cc1021/springcloud/FeignDeptConsumer_80.java
@@ -0,0 +1,18 @@
+package com.cc1021.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+
+/**
+ * Ribbon 和 Eureka 整合以后，客户端可以直接调用，不用关心IP地址和端口号
+ */
+@SpringBootApplication
+@EnableEurekaClient // eureka开启
+public class FeignDeptConsumer_80 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FeignDeptConsumer_80.class, args);
+    }
+}
