0bf213d11d67be942bb69707d82561f8b1e6f1a4
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-22 08:02:54 +0800

    添加dashboard

--- springcloud/springcloud-provider-dept-hystrix-8001/src/main/java/com/cc1021/springcloud/DeptProviderHystrix_8001.java
+++ springcloud/springcloud-provider-dept-hystrix-8001/src/main/java/com/cc1021/springcloud/DeptProviderHystrix_8001.java
@@ -1,10 +1,13 @@
 package com.cc1021.springcloud;
 
+import com.netflix.hystrix.contrib.metrics.eventstream.HystrixMetricsStreamServlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 /**
  * 启动类
@@ -17,5 +20,16 @@ public class DeptProviderHystrix_8001 {
 
     public static void main(String[] args) {
         SpringApplication.run(DeptProviderHystrix_8001.class, args);
+    }
+
+    /**
+     * 增加一个 Servlet
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean hystrixMetricsStreamServlet() {
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new HystrixMetricsStreamServlet());
+        servletRegistrationBean.addUrlMappings("/actuator/hystrix.stream");
+        return servletRegistrationBean;
     }
 }
