f16e68db1dd7b7e5c86ca9f94bfdfb6766ee9541
chengcheng1021 <chengcheng1021@aliyun.com>
2020-07-23 15:03:49 +0800

    客户端连接服务端访问远程

---
+++ springcloud/springcloud-config-client-3366/src/main/java/com/cc1021/springcloud/ConfigClient_3366.java
@@ -0,0 +1,12 @@
+package com.cc1021.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClient_3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3366.class, args);
+    }
+}
